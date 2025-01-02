7e2aa4bf2f5ee67213fb70657d1cafc3b2e69c39
CleitonRB <cleiton619@outlook.com>
2025-01-02 14:22:07 -0300

    Operações basicas List

---
+++ src/main/java/list/operacoesbasicas/Tarefa.java
@@ -0,0 +1,10 @@
+package main.java.list.operacoesbasicas;
+
+public class Tarefas {
+    //atributo
+    private String descricao;
+
+    public Tarefas(String descricao) {
+        this.descricao = descricao;
+    }
+}
