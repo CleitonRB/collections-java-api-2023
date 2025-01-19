1cd605c8ab44b6954f8d712c52c2b23bbac40bd9
CleitonRB <cleiton619@outlook.com>
2025-01-19 20:34:14 -0300

    finalizando catalogo de livros

--- src/main/java/list/pesquisa/CatalogoLivros.java
+++ src/main/java/list/pesquisa/CatalogoLivros.java
@@ -28,4 +28,44 @@ public class CatalogoLivros {
 
     return livrosPorAutor;
     }
+
+    public List<Livro> pesquisarPorIntervaloAnos(int anoInicial, int anoFinal){
+        List<Livro> livrosPorIntervaloAnos = new ArrayList<>();
+        if(!livroList.isEmpty()){
+            for(Livro l : livroList){
+                if(l.getAnoPublicacao() >= anoInicial && l.getAnoPublicacao() <= anoFinal){
+                    livrosPorIntervaloAnos.add(l);
+                }
+            }
+        }
+
+    return livrosPorIntervaloAnos;
+    }
+
+    public Livro pesquisarPorTitulo(String titulo){
+        Livro livroPorTitulo = null;
+        if(!livroList.isEmpty()){
+            for(Livro l : livroList){
+                if(l.getTitulo().equalsIgnoreCase(titulo)){
+                    livroPorTitulo = l;
+                    break;
+                }
+            }
+        }
+
+    return livroPorTitulo;
+    }
+
+    public static void main(String[] args) {
+        CatalogoLivros catalogoLivros = new CatalogoLivros();
+        catalogoLivros.adicionarLivro("Livro 1", "autor 1", 2020);
+        catalogoLivros.adicionarLivro("Livro 1", "autor 2", 2021);
+        catalogoLivros.adicionarLivro("Livro 2", "autor 2", 2022);
+        catalogoLivros.adicionarLivro("Livro 3", "autor 3", 2023);
+        catalogoLivros.adicionarLivro("Livro 4", "autor 4", 1994);
+
+        System.out.println(catalogoLivros.pesquisarPorAutor("autor 4"));
+        System.out.println(catalogoLivros.pesquisarPorIntervaloAnos(2020, 2021));
+        System.out.println(catalogoLivros.pesquisarPorTitulo("Livro 1"));
+    }
 }
