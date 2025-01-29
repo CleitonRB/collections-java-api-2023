3885d9bc576351d8e93f46cf016f00c8f0612687
CleitonRB <cleiton619@outlook.com>
2025-01-29 11:50:04 -0300

    Finalizando ordenacao

--- src/main/java/list/ordenacao/OrdenacaoPessoas.java
+++ src/main/java/list/ordenacao/OrdenacaoPessoas.java
@@ -1,27 +1,43 @@
 package main.java.list.ordenacao;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class OrdenacaoPessoas {
 
-    private String nome;
-    private int idade;
-    private double altura;
+    private List<Pessoas> pessoasList;
 
-    public OrdenacaoPessoas(String nome, int idade, double altura){
-        this.nome = nome;
-        this.idade = idade;
-        this.altura = altura; 
+    public void Pessoas(){
+        this.pessoasList = new ArrayList<>();
     }
 
-    public String getNome(){
-        return nome;
+    public void adicionarPessoa(String nome, int idade, double altura){
+        Pessoas pessoas = new Pessoas(nome, idade, altura);
+        this.pessoasList.add(pessoas);
     }
 
-    public int getIdade(){
-        return idade;
-    }
-
-    public double getAltura(){
-        return altura;
+    public List<Pessoas> ordenarPorIdade(){
+        List<Pessoas> pessoasPorIdade = new ArrayList<>(pessoasList);
+        Collections.sort(pessoasPorIdade);
+        return pessoasPorIdade;
     }
     
+    public List<Pessoas> ordenarPorAltura(){
+        List<Pessoas> pessoasPorAltura = new ArrayList<>(pessoasList);
+        Collections.sort(pessoasPorAltura, new ComparatorPorAltura());
+        return pessoasPorAltura;
+    }
+
+    public static void main(String[] args) {
+        OrdenacaoPessoas ordenacaoPessoas = new OrdenacaoPessoas();
+        
+        ordenacaoPessoas.adicionarPessoa("Nome 1", 20, 1.56);
+        ordenacaoPessoas.adicionarPessoa("Nome 2", 30, 1.80);
+        ordenacaoPessoas.adicionarPessoa("Nome 3", 25, 1.70);
+        ordenacaoPessoas.adicionarPessoa("Nome 4", 17, 1.56);
+
+        System.out.println(ordenacaoPessoas.ordenarPorIdade());
+        System.out.println(ordenacaoPessoas.ordenarPorAltura());
+    }
 }
--- src/main/java/list/ordenacao/Pessoas.java
+++ src/main/java/list/ordenacao/Pessoas.java
@@ -1,18 +1,52 @@
 package main.java.list.ordenacao;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Comparator;
 
-public class Pessoas {
+public class Pessoas implements Comparable<Pessoas>{
 
-    private List<OrdenacaoPessoas> ordenacaoPessoas;
+    private String nome;
+    private int idade;
+    private double altura;
 
-    public Pessoas(){
-        this.ordenacaoPessoas = new ArrayList<>();
+    public Pessoas(String nome, int idade, double altura){
+        this.nome = nome;
+        this.idade = idade;
+        this.altura = altura; 
     }
 
-    public void adicionarPessoa(String nome, int idade, double altura){
-        
+    public String getNome(){
+        return nome;
+    }
+
+    public int getIdade(){
+        return idade;
+    }
+
+    public double getAltura(){
+        return altura;
+    }
+
+    @Override
+    public String toString(){
+        return "Livro{" + 
+            "Nome ='" + nome + '\'' +
+            ", Idade ='" + idade + '\'' + 
+            ", Altura =" + altura + 
+            '}';
+    }
+
+    @Override
+    public int compareTo(Pessoas p) {
+        return Integer.compare(idade, p.getIdade());
+    }
+    
+}
+
+class ComparatorPorAltura implements Comparator<Pessoas>{
+
+    @Override
+    public int compare(Pessoas p1, Pessoas p2) {
+        return Double.compare(p1.getAltura(), p2.getAltura());
     }
     
 }
